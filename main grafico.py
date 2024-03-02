4ed6a8771e1b63d78a288391627114b1cd6bc779
diogo-duarte-sharkcoders <149233846+diogo-duarte-sharkcoders@users.noreply.github.com>
2024-03-02 10:28:17 +0000

    Update main grafico.py

--- main grafico.py
+++ main grafico.py
@@ -1,35 +1,31 @@
-from tkinter import*
+import tkinter as tk
 
 def was_clicked():
     print("a button was clicked.")
-def change_to_blue():
-    root.configure(bg=azul)
 
-def change_to_red():
-    root.configure(bg=vermelho)
+def change_color(color):
+    root.configure(bg=color)
+    label.configure(text=f"A cor mudou para {color}")
 
-def change_to_green():
-    root.configure(bg=verde)
-
-def change_to_black():
-    root.configure(bg=preto)
-azul= "#0087ff"
-vermelho= "#ff0000"
-verde= "#00ff00"
-preto= "#000000"
-
-root = Tk()
+root = tk.Tk()
 root.title("Janela")
 root.geometry("500x300+200+200")
 root.wm_resizable(width=False, height=False)
 
-button1 = Button(root, text="azul", command= change_to_blue, font="Time 20 bold")
-button1.place(width=80, height=30, x=0, y=0)
-button2 = Button(root, text="vermelho", command= change_to_red, font="Time 20 bold")
-button2.place(width=80, height=30, x=500-80, y=0)
-button3 = Button(root, text="verde", command= change_to_green, font="Time 20 bold")
-button3.place(width=80, height=30, x=0, y=300-30)
-button4 = Button(root, text="preto", command= change_to_black, font="Time 20 bold")
-button4.place(width=80, height=30, x=500-80, y=300-30)
+label = tk.Label(root, text="clica para mudar a cor", font=("Arial", 16))
+label.pack(pady=20)
+
+button1 = tk.Button(root, text="Red", command=lambda:change_color("Red"))
+button1.pack(pady=10)
+
+button2 = tk.Button(root, text="Blue", command=lambda:change_color("Blue"))
+button2.pack(pady=10)
+
+button3 = tk.Button(root, text="Green", command=lambda:change_color("Green"))
+button3.pack(pady=10)
+
+button4 = tk.Button(root, text="Black", command=lambda:change_color("Black"))
+button4.pack(pady=10)
 
 root.mainloop()
+
