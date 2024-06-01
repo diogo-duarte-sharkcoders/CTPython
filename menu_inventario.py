799f0436667f7ae7e8b5ac591bb154295286eb8e
diogo-duarte-sharkcoders <149233846+diogo-duarte-sharkcoders@users.noreply.github.com>
2024-06-01 09:14:40 +0100

    Update and rename inventário.py to menu_inventario.py

--- menu_inventario.py
+++ menu_inventario.py
@@ -1,6 +1,6 @@
 def menu_principal():
     while True:
-        registar_criar=input("1-Registar cliente\n2-Criar inventário\n3-Deletar biblioteca")
+        print("1-Registar cliente\n2-Criar inventário\n3-Apagar biblioteca")
         op = int(input("Insira uma opcão\n>"))
         if op == 1:
             input("Escreva o nome da biblioteca\n>")
@@ -8,10 +8,9 @@ def menu_principal():
             input("Escreva um nome para dar para a biblioteca\n>")
         elif op == 3:
             print("Tem acerteza?")
-            Deletar = input("y ou n")
+            Apagar = input("y ou n")
         else:
             print("Opcão inválida")
-
 
 
            
